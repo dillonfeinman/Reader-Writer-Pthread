0d63b48f44c78b38398f39475af3040f26bf43ed
Evan Moeller <evan@Evans-MacBook-Air.local>
2019-03-28 13:50:08 -0400

    Class templates

---
+++ reader.h
@@ -0,0 +1,11 @@
+#ifdef READER_H
+#define READER_H
+
+class Reader{
+private:
+
+public:
+    
+};
+
+#endif
---
+++ writer.h
@@ -0,0 +1,11 @@
+#ifdef WRITER_H
+#define WRITER_H
+
+class Writer{
+private:
+
+public:
+    
+};
+
+#endif
