770a9d014139c7f566a4013e20fe3ca97fc4de4a
Evan Moeller <evan@Evans-MacBook-Air.local>
2019-04-11 14:08:04 -0400

    Trimmed

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -1,7 +1,9 @@
 #include "LinkedList.h"
 #include <iostream>
 #include <cstdlib>
+#include <pthread.h>
 
+pthread_mutex_t wlock;
 
 LinkedList::LinkedList(){
 	this->current = this->head = NULL;
@@ -10,46 +12,37 @@ LinkedList::~LinkedList(){
 	current = head;
 	while(current != NULL){
 		delete current;
-		current = current->getNext();
+		current = current->next;
   }
 }
 void LinkedList::reset(){
 	this->current = this->head;
 }
 
-void LinkedList::print(){
-	this->reset();
-	while(current->getNext() != NULL){
-		std::cout << current->getData() << ", ";
-		current = current->getNext();
-	}
-	if(current != NULL){
-		std::cout << current->getData() << ".";
-	}
-	std::cout << std::endl;
-}
-
 void LinkedList::add(int x){
 	this->reset();
+	pthread_mutex_lock(&wlock);
 	if(head == NULL){
 		Node * n = new Node(x);
 		head = current = n;
+		pthread_mutex_unlock(&wlock);
 		return;
 	}
 	else {
 		Node * n = new Node(x);
-		while(current->getNext() != NULL){
-			current = current->getNext();
+		while(current->next != NULL){
+			current = current->next;
 		}
 		current->setNext(n);
-		current = current->getNext();
+		current = current->next;
 		current->setNext(NULL);
 	}
+	pthread_mutex_unlock(&wlock);
 	return;
 }
 bool LinkedList::next(){
-	if(current->getNext() != NULL){
-		this->current = this->current->getNext();
+	if(current->next != NULL){
+		this->current = this->current->next;
 		return true;
 	} else {
 		current = head;
@@ -67,20 +60,4 @@ Node::Node(int x, Node * n){
 }
 void Node::setNext(Node * n){
 	this->next = n;
-}
-Node * Node::getNext(){
-	if(this->next == NULL) return NULL;
-	else{
-		return this->next;
-	}
-}
-Node * LinkedList::getCurrent(){
-	return this->current;
-}
-int Node::getData(){
-	return this->data;
-
-}
-int LinkedList::getNodeData(){
-	return current->getData();
 }
