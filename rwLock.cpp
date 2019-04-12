2beb2f41ff39be7d635f28c03deff30fd440b89a
Evan Moeller <evan@Evans-MacBook-Air.local>
2019-04-11 20:40:20 -0400

    fixed bug

--- rwLock.cpp
+++ rwLock.cpp
@@ -19,7 +19,7 @@ void rwlock_release_readlock(rwlock_t *rw) {
     pthread_mutex_lock(&rw->baseLock);
     rw->readers--;
     if (rw->readers == 0)
-        pthread_cond_signal(&rw->writeLock, &rw->baseLock);
+        pthread_cond_signal(&rw->writeLock);
     pthread_mutex_unlock(&rw->baseLock);
 }
 
@@ -28,5 +28,5 @@ void rwlock_acquire_writelock(rwlock_t *rw) {
 }
 
 void rwlock_release_writelock(rwlock_t *rw) {
-    pthread_cond_signal(&rw->writeLock, &rw->baseLock);
+    pthread_cond_signal(&rw->writeLock);
 }
