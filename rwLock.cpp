091c6a9fc5af1f7404ae1cb3395693b74f8de7b3
Evan Moeller <evan@Evans-MacBook-Air.local>
2019-04-11 20:49:15 -0400

    Usable rw lock

--- rwLock.cpp
+++ rwLock.cpp
@@ -1,13 +1,7 @@
 #include <pthread.h>
+#include "rwLock.h"
 
-
-typedef struct _rwlock_t {
-    pthread_mutex_t baseLock = PTHREAD_COND_INITIALIZER;
-    pthread_cond_t writeLock = PTHREAD_MUTEX_INITIALIZER;
-    int readers;
-} rwlock_t;
-
-void rwlock_acquire_readlock(rwlock_t *rw) {
+void rwlock_t::rwlock_acquire_readlock(rwlock_t *rw) {
     pthread_mutex_lock(&rw->baseLock);
     rw->readers++;
     if(rw->readers == 1)
@@ -15,7 +9,7 @@ void rwlock_acquire_readlock(rwlock_t *rw) {
     pthread_mutex_unlock(&rw->baseLock);
 }
 
-void rwlock_release_readlock(rwlock_t *rw) {
+void rwlock_t::rwlock_release_readlock(rwlock_t *rw) {
     pthread_mutex_lock(&rw->baseLock);
     rw->readers--;
     if (rw->readers == 0)
@@ -23,10 +17,10 @@ void rwlock_release_readlock(rwlock_t *rw) {
     pthread_mutex_unlock(&rw->baseLock);
 }
 
-void rwlock_acquire_writelock(rwlock_t *rw) {
+void rwlock_t::rwlock_acquire_writelock(rwlock_t *rw) {
     pthread_cond_wait(&rw->writeLock, &rw->baseLock);
 }
 
-void rwlock_release_writelock(rwlock_t *rw) {
+void rwlock_t::rwlock_release_writelock(rwlock_t *rw) {
     pthread_cond_signal(&rw->writeLock);
 }
