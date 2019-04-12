61fb410f7f1eb4b2ad0753f6265dd7e22999a8eb
Evan Moeller <evan@Evans-MacBook-Air.local>
2019-04-12 16:57:48 -0400

    Test file

---
+++ main.cpp
@@ -0,0 +1,157 @@
+#include <pthread.h>
+#include <fstream>
+#include <string>
+#include <stdlib.h>
+
+using namespace std;
+
+struct node{
+    int val;
+    node *next;
+};
+
+struct llist{
+    node *head;
+    node *curr;
+};
+
+llist l;
+l.head = NULL;
+l.curr = NULL;
+
+int num;
+
+int reader = 0;
+int writer = 0;
+
+pthread_mutex_t rm = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t wm = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t rt = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t re = PTHREAD_MUTEX_INITIALIZER;
+
+void * read(void * in){
+    int *input = (int *) in;
+    int thr = *input+1;
+    int *count = (int *)malloc(sizeof(int)*num);
+    for(int i = 0; i < num; i++){
+        count[i]=0;
+    }
+    for(int i = 0; i < num; i++){
+        //Entry
+        pthread_mutex_lock(&rt);
+        pthread_mutex_lock(&rm);
+        reader++;
+        if(reader==1){
+            pthread_mutex_lock(&re);
+        }
+        pthread_mutex_unlock(&rm);
+        pthread_mutex_unlock(&rt);
+
+        //Critical section
+        cout << "r" << thr << endl;
+
+        //Read list
+        /*node *tmp = llist.head;
+        int k = 0;
+        while(tmpv!= NULL){
+            if((tmp->val%10) == thr){
+                count[k]++;
+            }
+            tmp = tmp->next;
+        }*/
+        //////////
+
+
+        //Exit
+        pthread_mutex_lock(&rm);
+        reader--;
+        if(reader==0){
+             pthread_mutex_unlock(&re);
+        }
+        pthread_mutex_unlock(&rm);
+    }
+
+    //Write output
+    string out = "reader_" + to_string(thr) + ".txt";
+
+    ofstream o (out);
+    for(int i = 0; i < num; i++){
+        out << "Reader " << thr << ": Read " << count[i] << " values ending in " << thr << endl; 
+    }
+    o.close();
+}
+
+void * write(void * in){
+    //Entry
+    pthread_mutex_lock(&wm);
+    writer++;
+    if(writer==1){
+        pthread_mutex_lock(&rt);
+    }
+    pthread_mutex_unlock(&wm);
+
+    //Critical section
+    pthread_mutex_lock(&re);
+    /*int *input = (int *) in;
+    int val;
+    for(;;){
+        val = rand()%1000 + 1;
+        if((val%10)==thr){
+            break;
+        }
+    }
+    l.curr = l.head;
+    while(l.curr->next != NULL){
+        l.curr = l.curr->next;
+    }*/
+    cout << "w" << thr << endl;
+
+
+    pthread_mutex_unlock(&re);
+
+    //Exit
+    pthread_mutex_lock(&wm);
+    writer--;
+    if(writer==0){
+        pthread_mutex_unlock(&rt);
+    }
+    pthread_mutex_unlock(&wm);
+}
+
+int main(int argc, char * argv[]){
+    if(argc != 4){
+      cerr << "Needs 3 arguments: <N rand ints> <R readers> <W writers>." << endl;
+      exit(1);
+    } else {
+        int n = stoi(argv[1]);
+	    num = n;
+        int r = stoi(argv[2]);
+        int w = stoi(argv[3]);
+        if(n < 1 || n > 1000 || r < 1 || r > 9 || w < 1 || w > 9){
+            cerr << "R and W must be between 1 and 9, inclusive, and N must be between 1 and 1000, inclusive." << endl;
+            exit(2);
+        } else {
+            pthread_t writers[w];
+            pthread_t readers[r];
+            
+            //Create threads
+            for(int i = 0; i < w; i++){
+                int *v = &i;
+                pthread_create(&writers[w], NULL, write, v);
+            }
+            for(int i = 0; i < r; i++){
+                int *v = &i;
+                pthread_create(&readers[r], NULL, read, v);
+            }
+
+            //rejoin threads
+            for(int i = 0; i < w; i++){
+                pthread_join(&writers[i], NULL);
+            }
+            for(int i = 0; i < r; i ++){
+                pthread_join(&readers[r], NULL);
+            }
+        }
+    }
+    return 0;
+}
